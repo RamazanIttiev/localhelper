32ed453312f69ed4caa62aa2f84b89ce51b66ea0
Ramazan Ittiev <rittiev@devexperts.com>
2023-02-07 11:14:09 +0530

    [1] create-react-app init and file structure refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './app/App';
 import reportWebVitals from './reportWebVitals';
+
+import { CssBaseline } from "@mui/material";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );

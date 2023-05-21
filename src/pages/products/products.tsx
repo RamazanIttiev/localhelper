af8bf0d013f743035b404df8fa3ca8a0e7c392b3
Ramazan Ittiev <rittiev@devexperts.com>
2023-05-21 12:00:26 +0300

    [113] main button visibility change

--- src/pages/products/products.tsx
+++ src/pages/products/products.tsx
@@ -54,7 +54,7 @@ export const Products = () => {
 	);
 
 	useEffect(() => {
-		if (!isCartEmpty && isRestaurantRoute && isRestaurantWorking) {
+		if (!isCartEmpty && isRestaurantRoute) {
 			showMainButton();
 			setMainButtonText('Order');
 			handleMainButton(navigateToCart);
